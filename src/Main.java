6e87406e4f8db672c68cd83043429bc715cc91d3
shinabarger <davidshinabarger@gmail.com>
2017-02-26 20:47:54 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * The while loop represents the game.
+ * Each iteration represents a turn of the game
+ * where you are given inputs (the heights of the mountains)
+ * and where you have to print an output (the index of the mountain to fire on)
+ * The inputs you are given are automatically updated according to your last actions.
+ **/
+class Player {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+
+        // game loop to shoot mountains forever..
+        while (true) {
+            int mountainToShoot = 0;
+            int highestHeight = 0;
+            int currentMountainHeight = 0;
+            for (int i = 0; i < 8; i++) {
+
+                currentMountainHeight = in.nextInt();
+                if (currentMountainHeight > highestHeight) {
+                    highestHeight = currentMountainHeight;
+                    mountainToShoot = i;
+                } // represents the height of one mountain.
+            }
+
+            // Write an action using System.out.println()
+            // To debug: System.err.println("Debug messages...");
+
+            System.out.println(mountainToShoot);
+            continue;
+            // The index of the mountain to fire on.
+        }
+    }
+}
